460d7c231dbb8891b9336345538ffb5db3649ae7
Max Br <supermaxick@gmail.com>
2021-11-17 14:38:08 +0300

    Initial commit

---
+++ src/com/company/testAuthor.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class testAuthor {
+    public static void main(String[] args){
+        Author a1 = new Author("aboba", "aboba@gmail.com",'M');
+        Author a2 = new Author("biba", "biba@gmail,com", 'F');
+        Author a3 = new Author("apace x-29", "apacex-29@gmail.com", 'H');
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+    }
+}
